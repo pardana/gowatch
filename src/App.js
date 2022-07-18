471d9becf87a1876c685835b618329649a9da846
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-18 20:57:13 +0700

    update section Time and Hear Rate button

--- src/App.js
+++ src/App.js
@@ -20,12 +20,17 @@ function App() {
           <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
           <p className={classes.ProductDescription}>{ProductData.description}</p>
 
-          <h3 className={classes.SectionHeading}>
+          <h3 className={classes.SectionHeading}>Select Color</h3>
             <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black Color Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Color Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Color Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Color Watch" />
-          </h3>
+
+            <h3 className={classes.SectionHeading}>Features</h3>
+            <div>
+              <button className={[classes.FeaturesItem, classes.SelectedFeaturesItem].join(' ')}>Time</button>
+              <button className={classes.FeaturesItem}>Heart Rate</button>
+            </div>
         </div>
       </div>
     </div>
