413c33b7833ad428b52a64bd514608b5e7088873
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-18 20:47:53 +0700

    update section product image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import classes from './App.module.css';
+import ProductData from './ProductData'
 
 function App() {
   return (
@@ -9,6 +10,24 @@ function App() {
           <img src='https://www.freepnglogos.com/uploads/amazon-png-logo-vector/amazon-symbol-png-logo-vector-9.png' alt='Amazon Logo' />
         </nav>
       </header>
+
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Review" />
+        </div>
+
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+          <h3 className={classes.SectionHeading}>
+            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black Color Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Color Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Color Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Color Watch" />
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
