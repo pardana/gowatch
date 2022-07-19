c294eff558b16cb75195ae243cebc2fcc4bd3c16
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-19 15:16:47 +0700

    update featureList dynamicly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React, { Component } from "react";
 import classes from "./App.module.css";
+import ProductData from "./ProductData";
 import ProductDetails from "./ProductDetails";
 import ProductPreview from "./ProductPreview";
 import Topbar from "./Topbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Topbar />
+class App extends Component {
+  state = {
+    productData: ProductData,
+  };
 
-      <div className={classes.MainContainer}>
-        <div className={classes.ProductPreview}>
-          <ProductPreview />
-        </div>
+  render() {
+    return (
+      <div className="App">
+        <Topbar />
 
-        <div className={classes.ProductData}>
-          <ProductDetails />
+        <div className={classes.MainContainer}>
+          <div className={classes.ProductPreview}>
+            <ProductPreview />
+          </div>
+
+          <div className={classes.ProductData}>
+            <ProductDetails data={this.state.productData} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,48 +1,46 @@
 import React from "react";
-import classes from './ProductDetails.module.css';
-import ProductData from "./ProductData";
+import classes from "./ProductDetails.module.css";
 
 export default function ProductDetails(props) {
+  const colorOptions = props.data.colorOptions.map((item, pos) => {
+    const classArr = [classes.ProductImage];
+    if (pos === 0) {
+      classArr.push(classes.SelectedProductImage);
+    }
+
+    return (
+      <img
+        key={pos}
+        className={classArr.join(" ")}
+        src={item.imageUrl}
+        alt={item.styleName}
+      />
+    );
+  });
+
+  const featureList = props.data.featureList.map((item, pos) => {
+    const classArr = [classes.FeaturesItem];
+    if (pos === 0) {
+      classArr.push(classes.SelectedFeaturesItem);
+    }
+
+    return (
+      <button key={pos} className={classArr.join(" ")}>
+        {item}
+      </button>
+    );
+  });
+
   return (
     <div className={classes.ProductData}>
-      <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-      <p className={classes.ProductDescription}>{ProductData.description}</p>
+      <h1 className={classes.ProductTitle}>{props.data.title}</h1>
+      <p className={classes.ProductDescription}>{props.data.description}</p>
 
       <h3 className={classes.SectionHeading}>Select Color</h3>
-      <img
-        className={[classes.ProductImage, classes.SelectedProductImage].join(
-          " "
-        )}
-        src="https://imgur.com/iOeUBV7.png"
-        alt="Black Color Watch"
-      />
-      <img
-        className={classes.ProductImage}
-        src="https://imgur.com/PTgQlim.png"
-        alt="Red Color Watch"
-      />
-      <img
-        className={classes.ProductImage}
-        src="https://imgur.com/Mplj1YR.png"
-        alt="Blue Color Watch"
-      />
-      <img
-        className={classes.ProductImage}
-        src="https://imgur.com/xSIK4M8.png"
-        alt="Purple Color Watch"
-      />
+      <div>{colorOptions}</div>
 
       <h3 className={classes.SectionHeading}>Features</h3>
-      <div>
-        <button
-          className={[classes.FeaturesItem, classes.SelectedFeaturesItem].join(
-            " "
-          )}
-        >
-          Time
-        </button>
-        <button className={classes.FeaturesItem}>Heart Rate</button>
-      </div>
+      <div>{featureList}</div>
 
       <button className={classes.PrimaryButton}>Buy Now</button>
     </div>
